51f9ab3a5e95fd0c5aaada43ce9af273adc473c9
Farush <98494033+Farush@users.noreply.github.com>
2022-04-04 12:17:17 +0300

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -38,11 +38,11 @@ while True:
 
 while True:
     seconds = input("Введите количество секунд: ")
-    if seconds.isdigit():
+    if seconds.isdigit() and int(number) > 0
         seconds = int(seconds)
         break
     else:
-        print("Вы ввели неверные данные, должно быть целое число. ")
+        print("Вы ввели неверные данные, должно быть целое положительное число. ")
 
 hours = seconds // 3600
 seconds = seconds % 3600
